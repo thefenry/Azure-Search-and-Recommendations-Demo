5d6a2ea31a516fe5879fdd8e4571e0a188359061
Ivan Sued <thefenry@gmail.com>
2017-02-13 14:59:07 -0800

    Adding Model and Controller

---
+++ Azure Search and Recommendations Demo/DAL/SearchContext.cs
@@ -0,0 +1,13 @@
+﻿using Azure_Search_and_Recommendations_Demo.Models;
+using System.Data.Entity;
+
+namespace Azure_Search_and_Recommendations_Demo.DAL
+{
+    public class SearchContext : DbContext
+    {
+        public SearchContext() : base("SearchContext")
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
