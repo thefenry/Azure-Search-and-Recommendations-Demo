333277bfe02d3417b4a4edabf661324f9d5b8bbd
Ivan Sued <thefenry@gmail.com>
2017-02-13 16:00:25 -0800

    add connectionstring and migration

---
+++ Azure Search and Recommendations Demo/Migrations/201702132329126_AddCarModel.cs
@@ -0,0 +1,29 @@
+namespace Azure_Search_and_Recommendations_Demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCarModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Make = c.String(),
+                        Model = c.String(),
+                        Year = c.Int(nullable: false),
+                        Rating = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cars");
+        }
+    }
+}
--- Azure Search and Recommendations Demo/Models/Car.cs
+++ Azure Search and Recommendations Demo/Models/Car.cs
@@ -1,14 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Search;
 
 namespace Azure_Search_and_Recommendations_Demo.Models
 {
     public class Car
     {
+        [Key]
+        [IsFilterable]
         public int Id { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable]
         public string Make { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
         public string Model { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
         public int Year { get; set; }
-        public int Double { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public int Rating { get; set; }
 
     }
 }
